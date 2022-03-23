969b716b8f4bd5d2a2136b24c897d102ec85b5af
komerica <jepilworld@gmail.com>
2022-03-22 23:58:57 -0400

    JS: Home contact button

--- main.js
+++ main.js
@@ -1,28 +1,40 @@
 "use scrict";
 
 // Make navbar transparent when it is on the top
-const navbar = document.querySelector("#navbar");
-const navbarHeight = navbar.getBoundingClientRect().height;
-//arrow function: not taking argument, execute the block inside
 document.addEventListener("scroll", () => {
   console.log(window.scrollY);
-  console.log(`navbarHeight: ${navbarHeight}`);
-  if (window.scrollY > navbarHeight) {
-    navbar.classList.add("navbar--dark");
-  } else {
-    navbar.classList.remove("navbar--dark");
-  }
 });
+
+// const navbar = document.querySelector("#navbar");
+// const navbarHeight = navbar.getBoundingClientRect().height;
+// //arrow function: not taking argument, execute the block inside
+// document.addEventListener("scroll", () => {
+//   console.log(window.scrollY);
+//   console.log(`navbarHeight: ${navbarHeight}`);
+//   if (window.scrollY > navbarHeight) {
+//     navbar.classList.add("navbar--dark");
+//   } else {
+//     navbar.classList.remove("navbar--dark");
+//   }
+// });
 
 //Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector(".navbar__menu");
-navbarMenu.addEventListener("click", (event) => {
-  const target = event.target;
-  const link = target.dataset.link;
+navbarMenu.addEventListener("click", (e) => {
+  const link = e.target.dataset.link;
   if (link == null) {
     return;
   }
-  console.log(event.target.dataset.link);
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({ behavior: "smooth" });
+  scrollIntoView(link);
 });
+
+//Handle click on "contact me" button on home
+const homeContactBtn = document.querySelector(".home__contact");
+homeContactBtn.addEventListener("click", () => {
+  scrollIntoView("#contact");
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+}
