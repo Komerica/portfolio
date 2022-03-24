0a9e985e5c1b46f0c19ea0880c82ca9e27bf7f39
komerica <jepilworld@gmail.com>
2022-03-23 23:57:41 -0400

    JS: Home transparent

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
 "use scrict";
 
-// Make navbar transparent when it is on the top
+/******************************************************/
+/**** Make navbar transparent when it is on the top ***/
+/******************************************************/
+const navbar = document.querySelector("#navbar");
+const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
   console.log(window.scrollY);
+  console.log(`navbarHeight: ${navbarHeight}`);
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add("navbar--dark");
+  } else {
+    navbar.classList.remove("navbar--dark");
+  }
 });
 
-// const navbar = document.querySelector("#navbar");
-// const navbarHeight = navbar.getBoundingClientRect().height;
-// //arrow function: not taking argument, execute the block inside
-// document.addEventListener("scroll", () => {
-//   console.log(window.scrollY);
-//   console.log(`navbarHeight: ${navbarHeight}`);
-//   if (window.scrollY > navbarHeight) {
-//     navbar.classList.add("navbar--dark");
-//   } else {
-//     navbar.classList.remove("navbar--dark");
-//   }
-// });
-
-//Handle scrolling when tapping on the navbar menu
+/*********************************************************/
+/**** Handle scrolling when tapping on the navbar menu ***/
+/*********************************************************/
 const navbarMenu = document.querySelector(".navbar__menu");
 navbarMenu.addEventListener("click", (e) => {
   const link = e.target.dataset.link;
@@ -28,12 +27,26 @@ navbarMenu.addEventListener("click", (e) => {
   scrollIntoView(link);
 });
 
-//Handle click on "contact me" button on home
+/****************************************************/
+/**** Handle click on "contact me" button on home ***/
+/****************************************************/
 const homeContactBtn = document.querySelector(".home__contact");
 homeContactBtn.addEventListener("click", () => {
   scrollIntoView("#contact");
 });
 
+/************************************************************************/
+/**** Make home slowly fade to transparent as the window scrolls down ***/
+/************************************************************************/
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+/*************************/
+/**** Utility Function ***/
+/*************************/
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: "smooth" });
