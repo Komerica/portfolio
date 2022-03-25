eb53f6749768ae27e2a6e70dad127bc584ac271d
komerica <jepilworld@gmail.com>
2022-03-24 23:57:35 -0400

    JS: Arrow up

--- main.js
+++ main.js
@@ -44,6 +44,25 @@ document.addEventListener("scroll", () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
+/***************************************************/
+/**** Show "Arrow up" button when scrolling down ***/
+/***************************************************/
+const arrowUp = document.querySelector(".arrow-up");
+document.addEventListener("scroll", () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.classList.add("visible");
+  } else {
+    arrowUp.classList.remove("visible");
+  }
+});
+
+/**********************************************/
+/**** Handle click on the "arrow up" button ***/
+/**********************************************/
+arrowUp.addEventListener("click", () => {
+  scrollIntoView("#home");
+});
+
 /*************************/
 /**** Utility Function ***/
 /*************************/
