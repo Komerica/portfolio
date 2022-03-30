99ba40b178cab8849fb8455b098e1f9841e59888
komerica <jepilworld@gmail.com>
2022-03-30 16:35:07 -0400

    JS: remove active from nav

--- main.js
+++ main.js
@@ -74,6 +74,13 @@ workBtnContainer.addEventListener("click", (e) => {
   if (filter == null) {
     return;
   }
+  //Remove selection from the previous item and select the new one.
+  const active = document.querySelector(".category__btn.selected");
+  active.classList.remove("selected");
+  const target =
+    e.target.nodeName === "BUTTON" ? e.target : e.target.parentNode;
+  target.classList.add("selected");
+
   projectContainer.classList.add("anim-out");
   setTimeout(() => {
     //Method1
