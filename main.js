83764642edd2b0a1cb0a164282120db3b9ae3690
komerica <jepilworld@gmail.com>
2022-03-31 23:19:39 -0400

    JS: Navbar menu toggle + project UI tweak

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 "use scrict";
 
-/******************************************************/
-/**** Make navbar transparent when it is on the top ***/
-/******************************************************/
+// Make navbar transparent when it is on the top
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
@@ -15,38 +13,41 @@ document.addEventListener("scroll", () => {
   }
 });
 
-/*********************************************************/
-/**** Handle scrolling when tapping on the navbar menu ***/
-/*********************************************************/
+// Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector(".navbar__menu");
 navbarMenu.addEventListener("click", (e) => {
   const link = e.target.dataset.link;
   if (link == null) {
     return;
   }
+  navbarMenu.classList.remove("open");
   scrollIntoView(link);
 });
 
-/****************************************************/
-/**** Handle click on "contact me" button on home ***/
-/****************************************************/
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
+navbarToggleBtn.addEventListener("click", () => {
+  navbarMenu.classList.toggle("open");
+});
+// Background color when clicked
+navbarToggleBtn.addEventListener("click", () => {
+  navbar.classList.toggle("bgcolor");
+});
+
+// Handle click on "contact me" button on home
 const homeContactBtn = document.querySelector(".home__contact");
 homeContactBtn.addEventListener("click", () => {
   scrollIntoView("#contact");
 });
 
-/************************************************************************/
-/**** Make home slowly fade to transparent as the window scrolls down ***/
-/************************************************************************/
+// Make home slowly fade to transparent as the window scrolls down
 const home = document.querySelector(".home__container");
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
-/***************************************************/
-/**** Show "Arrow up" button when scrolling down ***/
-/***************************************************/
+// Show "Arrow up" button when scrolling down
 const arrowUp = document.querySelector(".arrow-up");
 document.addEventListener("scroll", () => {
   if (window.scrollY > homeHeight / 2) {
@@ -56,16 +57,12 @@ document.addEventListener("scroll", () => {
   }
 });
 
-/**********************************************/
-/**** Handle click on the "arrow up" button ***/
-/**********************************************/
+// Handle click on the "arrow up" button
 arrowUp.addEventListener("click", () => {
   scrollIntoView("#home");
 });
 
-/*****************/
-/**** Projects ***/
-/*****************/
+// Projects
 const workBtnContainer = document.querySelector(".work__categories");
 const projectContainer = document.querySelector(".work__projects");
 const projects = document.querySelectorAll(".project");
